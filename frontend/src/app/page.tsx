4b441b7dadb4eb66e1f7633bcb6923de3bd27782
LUIZ GABRIEL RODRIGUES FREI <0031432412022@lti.fatecso.edu.br>
2025-09-17 16:48:07 -0300

    adicionado pagina de doação - incompleto

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,25 +1,12 @@
 import { Header } from "@/components/layout/Header";
+import Doacao from "./doacao";
 
 export default function HomePage() {
   return (
-    <div>
+    <>
       <Header />
-      <main style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        minHeight: '80vh',
-        textAlign: 'center'
-      }}>
-        <h1 style={{ fontSize: '2.5rem', fontWeight: 'bold' }}>
-          Bem-vindo ao Sistema de Doações
-        </h1>
-        <p style={{ fontSize: '1.2rem', marginTop: '1rem' }}>
-          Faça o login para continuar.
-        </p>
-      </main>
-    </div>
+      <Doacao />
+    </>
   );
 }
 
