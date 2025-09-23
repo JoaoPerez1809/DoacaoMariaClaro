b09c66ca7c78434675278a69ada825fec80ae23f
luizfrei <luizgrfrei@gmail.com>
2025-09-23 01:18:24 -0300

    Telas completas

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Header } from "@/components/layout/Header";
 import Doacao from "./doacao";
+import Cadastro from "./cadastro";  
+import Perfil from "./perfil";
+import Dashboard from "./dashboard";
 
 export default function HomePage() {
   return (
     <>
       <Header />
-      <Doacao />
+      <Dashboard />
     </>
   );
 }
