2a71c9407e0144ac2185e082dd47de22ebc6969d
luizfrei <luizgrfrei@gmail.com>
2025-09-25 19:45:48 -0300

    style(ui): ajuste de  estilos das telas que apresentavam bugs visuais

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Header } from "@/components/layout/Header";
-import Doacao from "./doacao";
+import DonationForm from "../components/doacao/DonationForm";
 import DashboardUsuarios from "../components/Dashboard/dashboard";
 import "@/app/globals.css"; // Certifique-se de que o caminho está correto
 import Perfil from "../components/Perfil/Profile";
 import Dashboard from "../components/Dashboard/dashboard";
+import { LoginForm } from "@/components/auth/LoginForm";
+import RegisterForm from "@/components/auth/RegisterForm";
 export default function HomePage() {
   return (
     <>
       <Header />
-      <Dashboard />
+      <RegisterForm/>
     </>
   );
 }
